162486ddec3eb2b37bf9bf2fb301bb96dbbfcd61
hossein <hrajaie20@gmail.com>
2024-05-19 13:23:19 +0330

    main.ts fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import locale from '@angular/common/locales/en';
+// import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+// import { NgbDatepickerConfig } from '@ng-bootstrap/ng-bootstrap';
+// jhipster-needle-angular-add-module-import JHipster will add new module here
+
+// import { ApplicationConfigService } from 'app/core/config/application-config.service';
+// import { fontAwesomeIcons } from './config/font-awesome-icons';
+// import MainComponent from './layouts/main/main.component';
 
 @Component({
-  selector: 'app-app',
+  selector: 'app-root',
   standalone: true,
-  imports: [],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: 'hi',
+  imports: [
+    // MainComponent
+  ],
 })
-export class AppComponent {
+export default class AppComponent {
+  // constructor(applicationConfigService: ApplicationConfigService, iconLibrary: FaIconLibrary, dpConfig: NgbDatepickerConfig) {
+  //   // applicationConfigService.setEndpointPrefix(SERVER_API_URL);
+  //   registerLocaleData(locale);
+  //   // iconLibrary.addIcons(...fontAwesomeIcons);
 
+  // }
 }
